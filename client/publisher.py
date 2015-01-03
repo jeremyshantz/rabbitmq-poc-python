0fe5813eb4a58fc54c9ce9c2748234559d26d5f7
Jeremy Shantz <jeremy@shantz.io>
2015-01-03 13:24:35 -0500

    added base64 encoding of the message body

--- client/publisher.py
+++ client/publisher.py
@@ -3,17 +3,40 @@
 #
 
 import pika
+import base64
 
 VERSION = '0.0.1'
 
 payload = '{"test": "hello, world"}'
 
+def buildmessage ():
+	data = base64.b64encode(payload)
+	return data
+
+
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
-channel.basic_publish(exchange='', routing_key='hello', body=payload)
+channel.basic_publish(exchange='', routing_key='hello', body=buildmessage())
 
 print " [x] Sent message"
 
 connection.close()
+
+
+# public class MessageV2
+# {
+# 	public string[] To { get; set; }
+# 	public string[] CC { get; set; }
+# 	public string[] BCC { get; set; }
+# 	public string Subject { get; set; }
+# 	public string Body { get; set; }
+# 	public List<AttachmentV2> Attachments { get; set; }
+# }
+
+#  public class AttachmentV2
+# {
+# 	public string FileName { get; set; }
+# 	public string Base64 { get; set; }
+# }
--- client/rabbitclient.py
+++ client/rabbitclient.py
@@ -4,21 +4,19 @@
 
 import pika
 import json
+import base64
 
 def callback(ch, method, properties, body):
-    print " [x] Received %r" % (body,)
-    obj = json.load(body)
-    print " [x] Received %r" % (obj['test'])
+    payload = base64.b64decode(body)
+    print " [x] Received %r" % (body) + ' ' + payload
+    obj = json.loads(payload)
+    print (obj['test'])
 
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
 
 channel.queue_declare(queue='hello')
-
-channel.basic_consume(callback,
-                      queue='hello',
-                      no_ack=True)
-
+channel.basic_consume(callback, queue='hello', no_ack=True)
 
 print ' [*] Waiting for messages. To exit press CTRL+C'
 channel.start_consuming()
